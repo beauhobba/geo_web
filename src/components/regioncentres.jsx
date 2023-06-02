fc50b9cfbf2c26dd1f694628f6a03c72b4582834
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-06-02 18:30:55 +1000

    Added: mesh block centroids

--- src/components/regioncentres.jsx
+++ src/components/regioncentres.jsx
@@ -1,40 +1,18 @@
-import React, { Text, useEffect  } from "react";
+import React, { Text, useEffect } from "react";
 
 import { Link } from "react-router-dom";
-import {Amplify, API, Auth} from 'aws-amplify'
+import { Amplify, API, Auth } from "aws-amplify";
 
-import awsconfig from '../aws-exports';
-import { Storage } from 'aws-amplify';
+import awsconfig from "../aws-exports";
+import { Storage } from "aws-amplify";
 
 import Coffee from "./coffee";
 
-// configure amplify
 // this can be added in index.js as well
 Amplify.configure(awsconfig);
-Auth.configure(awsconfig);
-
-
-
+// Auth.configure(awsconfig);
 
 function RegionCentres() {
-  // f.write('MB_CODE21\tLATITUDE\tLONGITUDE\tMB_CAT21
-  // \tLOCI_URI21\tAUS_CODE21\tGCC_CODE21\tGCC_NAME21\tAUS_NAME21
-  // \tCHG_LBL21\tCHG_FLAG21\tSTE_NAME21\tSTE_CODE21\tAREASQKM21\tSA1_CODE21\tSA2_CODE21\tSA2_NAME21\tSA3_CODE21\tSA3_NAME21\tSA4_CODE21\tSA4_NAME21\n')
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log('here')
-        let imageKeys = await Storage.list('')
-        console.log(imageKeys)
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-
   return (
     <div
       style={{
@@ -63,39 +41,74 @@ function RegionCentres() {
         }}
       >
         <div>
-          <h style={{ fontSize: 30 }}>Mesh Blocks</h>
+          <h style={{ fontSize: 30 }}>Mesh Block Centroids</h>
         </div>
-        <div>
-          <p>
-            Available data columns include:
+        <div style={{ flexDirection: "row", display: "flex" }}>
+          <div>
             <ul>
-              <li>MB_CODE21: The mesh block ID</li>
-              <li>LATITUDE</li>
-              <li>LONGITUDE</li>
-              <li>MB_CAT21</li>
-              <li>LOCI_URI21</li>
-              <li>AUS_CODE21</li>
-              <li>GCC_CODE21</li>
-              <li>GCC_NAME21</li>
-              <li>AUS_NAME21</li>
-              <li>CHG_LBL21</li>
-              <li>CHG_FLAG21</li>
-              <li>STE_NAME21</li>
-              <li>STE_CODE21</li>
-              <li>AREASQKM21</li>
-              <li>SA1_CODE21</li>
-              <li>SA2_CODE21</li>
-              <li>SA2_NAME21</li>
-              <li>SA3_CODE21</li>
-              <li>SA3_NAME21</li>
-              <li>SA3_NAME21</li>
-              <li>SA4_CODE21</li>
-              <li>SA4_NAME21</li>
+              <li>MB_CAT21: Category of the mesh block</li>
+              <li>
+                LOCI_URI21: ABS URI (Uniform Resource Identifier) for the
+                location
+              </li>
+              <li>AUS_CODE21: Code or identifier for Australia</li>
+              <li>
+                GCC_CODE21: Code or identifier for a geographical classification
+                system
+              </li>
+              <li>
+                GCC_NAME21: Name of the geographical classification system
+              </li>
+              <li>AUS_NAME21:Australia</li>
+              <li>CHG_LBL21: Change value</li>
+              <li>CHG_FLAG21: Flag or indicator associated with a change</li>
+              <li>STE_NAME21: Name of a state or province</li>
             </ul>
-          </p>
-          <Link to="/Downloads/mesh_centres.txt" target="_blank" download>.txt</Link>
-          <Link to="/Downloads/mesh_centres.xlsx" target="_blank" download>.xlsx</Link>
-        </div>
+          </div>
+          <div>
+            <ul>
+              <li>STE_CODE21: Code or identifier for a state or province</li>
+              <li>AREASQKM21: Area measurement in square kilometers</li>
+              <li>
+                SA1_CODE21: Code or identifier for Statistical Area Level 1
+                (SA1)
+              </li>
+              <li>
+                SA2_CODE21: Code or identifier for Statistical Area Level 2
+                (SA2)
+              </li>
+              <li>SA2_NAME21: Name of Statistical Area Level 2 (SA2)</li>
+              <li>
+                SA3_CODE21: Code or identifier for Statistical Area Level 3
+                (SA3)
+              </li>
+              <li>SA3_NAME21: Name of Statistical Area Level 3 (SA3)</li>
+              <li>SA3_NAME21: (Duplicate entry, please verify)</li>
+              <li>
+                SA4_CODE21: Code or identifier for Statistical Area Level 4
+                (SA4)
+              </li>
+              <li>SA4_NAME21: Name of Statistical Area Level 4 (SA4)</li>
+            </ul>
+          </div>
+          </div>
+          <div>
+          <Link
+            to="https://geo145327-staging.s3.ap-southeast-2.amazonaws.com/public/mesh_centres.txt"
+            target="_blank"
+            download
+          >
+            .txt
+          </Link>
+          &emsp;
+          <Link
+            to="https://geo145327-staging.s3.ap-southeast-2.amazonaws.com/public/mesh_centres.xlsx?AWSAccessKeyId=ASIAYCCKBJRX6DYZWTQP&Expires=1685693999&Signature=mpu4qohV%2Fpet1bta3BgMHMI7cU0%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEJj%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0yIkcwRQIhAOWVRehAQ3p4LeXBBiSFEYF7i6CQ8jLzTOoGrTFgaeCcAiAYg%2BCdoaZneFQODmPddCvyxFr4%2BbFhPxTFjXOf63K4dCrrBAjR%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAMaDDU1NDIwNjA1NTUzNSIMzY3UbQBwi2mIQNivKr8EnImbriLMC1Xco6rP4DQimd6w1ZmWXBWl8PMDKAhMRgSzXAwrmGsz6UTiV%2BXlNviglS3S30jYNsuZaMuls79wCRv%2BuNb2ZVVheVpC6JLjZf1HyHMh0lrm4ATFXzKqECL0sEEeU%2FLnqHq9%2BEj2l4uJ8N7kW%2BqJ9CYuhhrO0F6CiJ7rlLrZNNglWx3bOnuL6RKqhb0hF3sqm%2BQRNYSHSpFXHfCUplr1Jh0ATSKhTzxud7%2F1jKzRfpPnJ7sJT1ToKVy%2F%2FlAV5z5by2Xsml4oocnmxG3HoexloAbFovJJog3hNkFobJvZVvRAj24KRdHsjxe%2Bp9fu4DlsaHWmGOnSwo80pWR9XWS181eR%2FT%2FZsJUQ9JSsvB0nKbaJW0MUezT3sB9nrgpfP1vkwOp%2BidVGkk%2Bf%2BsG1kai4ORO3JN%2Bz9QLgh%2B9CI4wYlPSZbaaSuq4uref0HuVHgQWZE7HybdQNFIhIVeMJrQ9H3CDga7%2FIGvt3dI9tlwK5WDZ0Upk%2Fl3%2FZR0s7DHCmmPYlGqjqxuLKcVav5TmLQ%2FO1Ic36G5yl6wqEXV3oBPNAWbi28rXcHz0Pe6foxPiRj3vLcg8JhvU2ssQRXgkWDGHN%2F7vgqEcbDQD6KCPl%2B0e69pCp6Kk%2FE0dsIhf6drDm50VP26G1OzGj8QU0Acvac8mrMr9%2F5QjdOJ0jDbTFeXxP5fGZWyuTzg4UJ%2FBbK6seIYn1LSR4Y6VOJUPjKlkgcSr18BMDcduSmW4U3swv2ysniKiIlcmZXIYAcNgw6rbmowY6hQIxmLsR56HdfKFGQo9wvXofpgjL%2F9gQnXsB%2F0B6hk1KKuQ5dbrOKT9mq7vO5ZyuHu4yytbPF1vhkXnhoWPCaELRVrWq%2FXdKKrFz5zaKhvzOXyrZEhzXXD88vFMqL8VLT0D93%2Bne32S%2F3dGB%2BFHtB3pjHwTwOjnHKl7dDlGHlxZlnBcxvaXUBz29GvJO%2F5IFOnX0jW70U8ODMDv0m0rqFUDmHZzjgxluGbIAFgcM9auUz1sg%2B9FYoaZljq%2FUfnOlCoBRjwOACGnBy6gBejGS6flvPvddgzrO3Or77ZD8WdHRe3q1lArkn8Yqemh%2F7q1uPXF%2BJ8lRgVC5Vufb83jwBCGTOqmCnZQ%3D"
+            target="_blank"
+            download
+          >
+            .xlsx
+          </Link>
+          </div>
       </div>
     </div>
   );
