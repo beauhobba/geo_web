707ea5a3a83f517edaaff9a47b5a21093f393ba4
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-06-05 10:36:49 +1000

    Added: smoother map translations

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -13,86 +13,89 @@ function MapComponent() {
 
   useEffect(() => {
     API.post(myAPI, "/items", { body: {} })
-      .then((mapboxapi_res) => {
-        setMapboxApi(mapboxapi_res);
-        fetch(g_data)
-          .then((response) => response.text())
-          .then((data) => {
-            const parsedData = Papa.parse(data, { header: true }).data;
-            const lga = parsedData.filter(
-              (row) => row["LATITUDE"] && row["LONGITUDE"]
-            );
-
-            const lga_centres_latitudes = lga.map((row) =>
-              parseFloat(row["LATITUDE"].replace(",", "."))
-            );
-            const lga_centres_longitudes = lga.map((row) =>
-              parseFloat(row["LONGITUDE"].replace(",", "."))
-            );
-            const lga_centre_names = lga.map((row) => row["LGA_NAME22"]);
-
-            if (mapboxapi != null) {
-              const trace1 = {
-                type: "scattermapbox",
-                lat: lga_centres_latitudes,
-                lon: lga_centres_longitudes,
-                hovertext: lga_centre_names,
-                mode: "markers",
-                marker: {
-                  size: 5,
-                  color: colors.txt_2,
-                },
-                text: lga_centre_names,
-              };
-
-              const layout = {
-                title: {
-                  text: "LGA Centres Australia",
-                  font: {
-                    size: 24,
-                    font: "sans-serif",
-                  },
-                },
-                mapbox: {
-                  center: {
-                    lat: -28,
-                    lon: 133.4,
-                  },
-                  style: "dark",
-                  accesstoken: mapboxapi,
-                  zoom: 3.4,
-                },
-                autosize: true,
-                height: 650,
-                paper_bgcolor: "#000000", // Set the background color of the entire chart to black
-                plot_bgcolor: "#000000", // Set the background color of the plot area to black
-                margin: {
-                  l: 0,
-                  r: 0,
-                  t: 40,
-                  b: 0,
-                },
-                legend: {
-                  bgcolor: "rgba(255, 255, 255, 0.7)",
-                  bordercolor: "rgba(0, 0, 0, 0.2)",
-                  borderwidth: 1,
-                  x: 0,
-                  y: 1,
-                  orientation: "v",
-                },
-              };
-
-              Plotly.newPlot("map", [trace1], layout);
-            }
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          });
+      .then((response) => {
+        setMapboxApi(response);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    if (mapboxapi) {
+      fetch(g_data)
+        .then((response) => response.text())
+        .then((data) => {
+          const parsedData = Papa.parse(data, { header: true }).data;
+          const lga = parsedData.filter(
+            (row) => row["LATITUDE"] && row["LONGITUDE"]
+          );
+
+          const lga_centres_latitudes = lga.map((row) =>
+            parseFloat(row["LATITUDE"].replace(",", "."))
+          );
+          const lga_centres_longitudes = lga.map((row) =>
+            parseFloat(row["LONGITUDE"].replace(",", "."))
+          );
+          const lga_centre_names = lga.map((row) => row["LGA_NAME22"]);
+
+          const trace1 = {
+            type: "scattermapbox",
+            lat: lga_centres_latitudes,
+            lon: lga_centres_longitudes,
+            hovertext: lga_centre_names,
+            mode: "markers",
+            marker: {
+              size: 5,
+              color: colors.txt_2,
+            },
+            text: lga_centre_names,
+          };
+
+          const layout = {
+            title: {
+              text: "LGA Centres Australia",
+              font: {
+                size: 24,
+                font: "sans-serif",
+              },
+            },
+            mapbox: {
+              center: {
+                lat: -28,
+                lon: 133.4,
+              },
+              style: "dark",
+              accesstoken: mapboxapi,
+              zoom: 3.4,
+            },
+            autosize: true,
+            height: 650,
+            paper_bgcolor: "#000000", // Set the background color of the entire chart to black
+            plot_bgcolor: "#000000", // Set the background color of the plot area to black
+            margin: {
+              l: 0,
+              r: 0,
+              t: 40,
+              b: 0,
+            },
+            legend: {
+              bgcolor: "rgba(255, 255, 255, 0.7)",
+              bordercolor: "rgba(0, 0, 0, 0.2)",
+              borderwidth: 1,
+              x: 0,
+              y: 1,
+              orientation: "v",
+            },
+          };
+
+          Plotly.newPlot("map", [trace1], layout);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }, [mapboxapi]);
 
   return <div id="map"></div>;
 }
