e3a40d0ad8c6d372bdf93dc84ecb345acb03dac2
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-06-10 21:35:55 +1000

    added: sitemap xml

---
+++ src/sitemap-generator.js
@@ -0,0 +1,17 @@
+require("babel-register")({
+    presets: ["es2015", "react"]
+  });
+   
+
+  const router = require("./routes").default;
+  const Sitemap = require("react-router-sitemap").default;
+  
+  function generateSitemap() {
+      return (
+        new Sitemap(router)
+            .build("https://www.australiadatabases.com")
+            .save("./public/sitemap.xml")
+      );
+  }
+  
+  generateSitemap();
